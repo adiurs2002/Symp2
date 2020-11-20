c5f75af7da035a42a37075d5ed7b0503f83ebff9
adiurs2002 <adiurs2002@gmail.com>
2020-11-20 19:59:02 +0530

    bottom nav done!!!

--- app/src/main/java/com/example/symp2/MainActivity.kt
+++ app/src/main/java/com/example/symp2/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.symp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val firstFragment = HomeFragment()
+        val secondFragment = MedicFragment()
+        val thirdFragment = ProfileFragment()
+
+        setCurrentFragment(firstFragment)
+
+
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
 }
